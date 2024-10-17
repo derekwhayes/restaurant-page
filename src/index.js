510926761a0d7b334c6aa75127b9f0a9534959f0
Derek Hayes <derek2099@gmail.com>
2024-10-17 05:14:05 -0500

    Add navigation functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import { home } from "./home.js";
 import { menu } from "./menu.js";
 import { about } from "./about.js";
 
-// home();
-// menu();
-// about();
+home();
+
+const navigationController = (() => {
+    const content = document.querySelector('#content');
+    const homeBtn = document.querySelector('#home-btn');
+    const menuBtn = document.querySelector('#menu-btn');
+    const aboutBtn = document.querySelector('#about-btn');
+
+    homeBtn.addEventListener('click', () => {
+        content.innerHTML = '';
+        home();
+    });
+    menuBtn.addEventListener('click', () => {
+        content.innerHTML = '';
+        menu();
+    });
+    aboutBtn.addEventListener('click', () => {
+        content.innerHTML = '';
+        about();
+    });
+})();
